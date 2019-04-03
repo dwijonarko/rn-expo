3aa3344adc9c1c94e4c859903f7594fdc6c215bd
dwi wijonarko <d.wijonarko@gmail.com>
2019-04-03 19:29:06 +0700

    pertemuan9

--- navigations/DrawerNavigator.js
+++ navigations/DrawerNavigator.js
@@ -1,20 +1,63 @@
-import { createAppContainer,createDrawerNavigator } from "react-navigation";
+import React, { Component } from 'react';
+
+import { Dimensions,Platform } from 'react-native';
+import { createAppContainer, createDrawerNavigator, createBottomTabNavigator,createStackNavigator   } from "react-navigation";
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import SettingScreen from '../screens/SettingScreen';
+import MenuDrawer from '../components/MenuDrawer';
+import CameraScreen from '../screens/CameraScreen';
+
+const WIDTH = Dimensions.get('window').width;
+
+const DrawerConfig = {
+    drawerWidth:WIDTH*0.75 ,
+    contentComponent:({navigation}) =>{
+        return (<MenuDrawer navigation={navigation} />)
+    }
+}
+
+const Tabs = createBottomTabNavigator({
+    Home: HomeScreen,
+    About: ProfileScreen,
+    Contact: SettingScreen,
+}, {
+    tabBarOptions: {
+        activeTintColor: '#000',
+        inactiveTintColor: 'gray',
+        style: {
+            backgroundColor: '#fff',
+        },
+        indicatorStyle: {
+            backgroundColor: '#000',
+        },
+    }
+});
 
 const DrawerNavigator = createDrawerNavigator(
     {
         Home:{
-            screen: HomeScreen
+            screen: Tabs
         },
         Profile: {
             screen: ProfileScreen
         },
         Setting: {
             screen: SettingScreen
-        }
-    }
+        },
+        Camera: {
+            screen: CameraScreen
+        },
+    },
+    DrawerConfig
 );
 
-export default createAppContainer(DrawerNavigator);
+const StackNavigator = createStackNavigator({
+    DrawerNavigator: {
+        screen: DrawerNavigator,
+        navigationOptions: {
+            header: null,
+        }
+    }
+});
+export default createAppContainer(StackNavigator);
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,25 +1,125 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MenuButton from '../components/MenuButton';
-export default class ProfileScreen extends React.Component {
+import React, { Component } from 'react';
+import { ImageBackground, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, Permissions,FileSystem } from 'expo';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+class ProfileScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hasCameraPermission: null,
+            imageSrc: null,
+            type: Camera.Constants.Type.back,
+        };
+    }
+
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+    }
+
+    handleSavePress= () =>{
+        FileSystem.moveAsync({
+            from: this.state.imageSrc,
+            to: FileSystem.documentDirectory + 'Downloads/imagename.png'
+        });
+    }
+
+    handleSwitchCameraPress = () => {
+        this.setState({
+            type: this.state.type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back,
+        });
+    };
+
+    handleTakePicturePress = async () => {
+        const photo = await this.camera.takePictureAsync({ base64: true, quality: 0.9 });
+        const imageSrc = 'data:image/jpg;base64,' + photo.base64;
+        this.setState({ imageSrc });
+    };
+
     render() {
+        const { hasCameraPermission, imageSrc, type } = this.state;
+        if (hasCameraPermission === null) {
+            return null;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        }
+        if (imageSrc) {
+            return (
+                <ImageBackground
+                    resizeMode="stretch"
+                    source={{ uri: imageSrc }}
+                    style={{
+                        flex: 1,
+                        justifyContent: 'flex-end',
+                    }}
+                >
+                    <View style={{
+                        flex: 0.1,
+                        flexDirection: 'row',
+                        justifyContent: 'space-evenly',
+                    }}
+                    >
+                    <TouchableOpacity onPress={() => this.setState({ imageSrc: null })} style={{}}>
+                        <Ionicons
+                            name="md-trash"
+                            color="white"
+                            size={32}
+                        // onPress={() => this.props.navigation.toggleDrawer()}
+                        />
+                    </TouchableOpacity>
+                        <TouchableOpacity onPress={() => this.handleSavePress()} style={{}}>
+                        <Ionicons
+                            name="md-save"
+                            color="white"
+                            size={32}
+                        // onPress={() => this.props.navigation.toggleDrawer()}
+                        />
+                    </TouchableOpacity>
+                    </View>
+                </ImageBackground>
+                
+            );
+        }
         return (
-            <View style={styles.container}>
-                <MenuButton navigation={this.props.navigation} />
-                <Text style={styles.text}>Profile</Text>
-            </View>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Camera
+                    ref={(ref) => (this.camera = ref)}
+                    style={{
+                        flex: 1,
+                        justifyContent: 'flex-end',
+                    }}
+                    type={type}
+                >
+                    <View style={{
+                        flex: 0.1,
+                        flexDirection: 'row',
+                        justifyContent: 'space-evenly',
+                    }}
+                    >
+                        <TouchableOpacity onPress={this.handleTakePicturePress} style={{}}>
+                            <Ionicons
+                                name="md-camera"
+                                color="white"
+                                size={32}
+                                // onPress={() => this.props.navigation.toggleDrawer()}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.handleSwitchCameraPress} style={{}}>
+                            <Ionicons
+                                name="md-refresh"
+                                color="white"
+                                size={32}
+                                // onPress={() => this.props.navigation.toggleDrawer()}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
+            </SafeAreaView>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 30,
-    }
-});
+export default ProfileScreen;
